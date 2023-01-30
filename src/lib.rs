8d67da21eb5378b465f27b4f960575f0b647412c
MSILycanthropy <53885212+MSILycanthropy@users.noreply.github.com>
2023-01-29 21:26:57 -0600

    move eat die repeat (#1)
    * move eat die repeat
    
    * cringe s code

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,10 @@ use bevy::prelude::*;
 pub mod game;
 pub mod level;
 pub mod menu;
+pub mod snake;
 
 pub const SCALE: f32 = 16.0;
-
-#[derive(Component)]
-pub struct Player {
-    pub direction: PlayerDirection,
-    pub movement_timer: Timer,
-}
+pub const FPS: f64 = 60.0;
 
 #[derive(Clone, Copy, Eq, PartialEq, Debug, Hash)]
 pub enum PlayerDirection {
@@ -20,22 +16,25 @@ pub enum PlayerDirection {
     Right,
 }
 
-impl PlayerDirection {
-    fn to_vec(&self) -> Vec3 {
-        match self {
-            Self::Up => Vec3::new(0.0, 1.0, 0.0),
-            Self::Down => Vec3::new(0.0, -1.0, 0.0),
-            Self::Left => Vec3::new(-1.0, 0.0, 0.0),
-            Self::Right => Vec3::new(1.0, 0.0, 0.0),
-        }
-    }
-}
-
 #[derive(Clone, Eq, PartialEq, Debug, Hash)]
 pub enum GameState {
     Menu,
     Playing,
     GameOver,
+}
+
+#[derive(Component, Clone, Copy, Eq, PartialEq)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+pub struct Frames(pub u32);
+
+impl Into<f32> for Frames {
+    fn into(self) -> f32 {
+        (self.0 as f32) / (FPS as f32)
+    }
 }
 
 pub fn despawn<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
